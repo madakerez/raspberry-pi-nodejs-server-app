5725eab3bbda3a4ae1f0566b0fa87e6a79ee902f
Adam <adamzerek@gmail.com>
2017-04-23 11:56:08 +0200

    removed mocked gpio object

--- app/controllers/pinsManager.js
+++ app/controllers/pinsManager.js
@@ -1,43 +1,34 @@
-//var gpio = require('rpi-gpio');
-
-var gpio = {
-  setup: function() {
-
-  },
-  write: function() {
-
-  }
-}
+var gpio = require('rpi-gpio');
 
 class PinsManager {
-  constructor() {
-    this.activePins = [];
-  }
+	constructor() {
+		this.activePins = [];
+	}
 
-  getPinState(pin) {
-    let pinIndex = this.activePins.indexOf(pin);
-    if (pinIndex > -1) {
-      return 'HIGH'
-    } else {
-      return 'LOW'
-    }
-  }
-
-  managePinState(pin) {
-    let pinState = 1;
-    let pinIndex = this.activePins.indexOf(pin);
+	getPinState(pin) {
+		let pinIndex = this.activePins.indexOf(pin);
 		if (pinIndex > -1) {
-    		this.activePins.splice(pinIndex, 1);
-    		pinState = 0;
+			return 'HIGH'
+		} else {
+			return 'LOW'
+		}
+	}
+
+	managePinState(pin) {
+		let pinState = 1;
+		let pinIndex = this.activePins.indexOf(pin);
+		if (pinIndex > -1) {
+			this.activePins.splice(pinIndex, 1);
+			pinState = 0;
 		} else {
 			this.activePins.push(pin);
 		}
-	  gpio.setup(pin, gpio.DIR_OUT,() => {
-  	   gpio.write(pin, pinState, () => {
-        console.log(`pin ${pin} change value to ${pinState}`);
-      });
+		gpio.setup(pin, gpio.DIR_OUT, () => {
+			gpio.write(pin, pinState, () => {
+				console.log(`pin ${pin} change value to ${pinState}`);
+			});
 		});
-  }
+	}
 }
 
 module.exports = PinsManager;
